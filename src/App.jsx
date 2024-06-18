570629fac29a50798a952e98d0e68236711a585e
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-18 20:44:17 +0530

    iPhone model in the screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
+import Model from "./components/Model"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
